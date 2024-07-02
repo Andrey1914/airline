8071f66b5839c2448d906299c411fad5c0ea743c
Andrey1914 <anlikajw@gmail.com>
2024-07-02 22:14:17 +0200

    created users app for authentication

--- AIRLINE/urls.py
+++ AIRLINE/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("flights/", include("FLIGHTS.urls")),
+    path('flights/', include("FLIGHTS.urls")),
+    path('users/', include("USERS.urls"))
 ]
